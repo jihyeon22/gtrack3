53021a81cb9daec17b984ff1be9524bad9d8f166
kksworks <kksworks@gmail.com>
2017-05-18 17:21:08 +0900

    [model] kt fota fix

--- ext/protocol/kt_fota/kt_fota_svc/kt_dmc_proc.c
+++ ext/protocol/kt_fota/kt_fota_svc/kt_dmc_proc.c
@@ -8,7 +8,7 @@
 
 #define LOG_TARGET eSVC_BASE
 
-
+#define KT_DMC_BIN_PATH	"/kt_dms/bin/kt_dmc"
 int kt_dmc_proc_for_tl500k(char* buf)
 {
 	// APPS_AT$$KT_DMC_EXECUTE=E3087F6B0C0EE24A28C854302A60464702C800000028DB094B545349445F303031,
@@ -42,7 +42,14 @@ int kt_dmc_proc_for_tl500k(char* buf)
     if(tr == NULL) return -1;
 //	printf(" >>>> tr is [%s]\r\n", tr);
 	
-	// argument "-m"
+	// arument 0 : KT_DMC_BIN_PATH
+	prog_argv[prog_argv_index] = malloc(strlen(KT_DMC_BIN_PATH) + 1);
+	memset(prog_argv[prog_argv_index], 0, strlen(KT_DMC_BIN_PATH) + 1);
+	strncpy(prog_argv[prog_argv_index], KT_DMC_BIN_PATH, strlen(KT_DMC_BIN_PATH));
+
+	prog_argv_index ++;
+
+	// arument 1 :  "-m"
 	prog_argv[prog_argv_index] = malloc(strlen("-m") + 1);
 	memset(prog_argv[prog_argv_index], 0, strlen("-m") + 1);
 	strncpy(prog_argv[prog_argv_index], "-m", strlen("-m"));
@@ -69,15 +76,20 @@ int kt_dmc_proc_for_tl500k(char* buf)
 
 	if ( prog_argv_index > 1 )
 	{
-		mds_api_system_fork("/kt_dms/bin/kt_dmc",1, prog_argv);
+		mds_api_system_fork(KT_DMC_BIN_PATH, 1, prog_argv);
 	}
-	else
+
+	// free for argument..
+	for ( i = 0 ; i < prog_argv_index ; i ++ )
+	{
+		free(prog_argv[i]);
+	}
+
+	if ( prog_argv_index < 1 )
 	{
 		LOGE(LOG_TARGET, "[KT FOTA SVC] error : no argument \r\n");
 		return -1;
 	}
-
-
 		
 	return 1;
 }
