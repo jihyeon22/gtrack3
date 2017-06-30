6e41e2485f6d4dc040e5b08960a29a03159f34d4
kksworks <kksworks@gmail.com>
2017-06-30 16:44:56 +0900

    [model - netio] init commit

---
+++ model/netio/build.mk
@@ -0,0 +1,11 @@
+include ext/protocol/mdt800/mdt800.mk
+
+OBJS_MODEL	+= $(MODEL_PATH)/netcom.o
+OBJS_MODEL	+= $(MODEL_PATH)/callback.o
+
+OBJS_MODEL	+= $(MODEL_PATH)/config.o
+OBJS_MODEL	+= $(MODEL_PATH)/data-list.o
+OBJS_MODEL	+= $(MODEL_PATH)/validation.o
+
+OBJS_MODEL	+= $(MODEL_PATH)/at_noti.o
+OBJS_MODEL	+= $(MODEL_PATH)/thread-keypad.o
