a7d76d6fd0dd80d8d22f5a3d56194b9added093e
kksworks <kksworks@gmail.com>
2017-07-28 10:57:49 +0900

    [model - neognp] btn event add

--- model/hnrt/custom.h
+++ model/hnrt/custom.h
@@ -3,7 +3,17 @@
 
 typedef enum msg_ev_code_custom msg_ev_code_custom_t;
 enum msg_ev_code_custom{
-	eMAX_EVENT_CODE                 = 39, //LAST_EVENT_CODE + 1
+
+#if defined(SERVER_ABBR_NEO)
+	/* GEO FENCE NUM #2 entry Event Code */
+	eNEO_MDM_BTN1_EVT       = 40,
+	/* GEO FENCE NUM #2 exit Event Code */
+	eNEO_MDM_BTN2_EVT       = 40,
+	eMAX_EVENT_CODE           = 43, //LAST_EVENT_CODE + 1
+#else
+	eMAX_EVENT_CODE           = 39, //LAST_EVENT_CODE + 1
+#endif
+	
 };
 
 #define MILEAGE_FILE	"/data/mds/data/mdt800_hnrt_mileage.dat"
