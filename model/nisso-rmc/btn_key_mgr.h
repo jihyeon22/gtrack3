f34f6ffee25cb8cf5386882c79857628a78e86c7
kksworks <kksworks@gmail.com>
2018-04-24 14:44:34 +0900

    [model : nisso-rmc] gpio ext btn

---
+++ model/nisso-rmc/btn_key_mgr.h
@@ -0,0 +1,11 @@
+#ifndef __BTN_KEY_MGR_H__
+#define __BTN_KEY_MGR_H__
+
+#define NISSO_EXT_GPIO_BTN_GPIO_NUM_1  14
+#define NISSO_EXT_GPIO_BTN_GPIO_NUM_2  13
+
+#define CHECK_EVENT_FILERING_SEC       1
+
+void nisso_btn_mgr__init();
+
+#endif
