7236fc4f75d4aed326effd65a20db5e93ec0657e
kksworks <kksworks@gmail.com>
2018-06-22 10:38:52 +0900

    [model - bizincar mdt] compile bug fix

--- model/bizincar/callback.c
+++ model/bizincar/callback.c
@@ -491,3 +491,11 @@ void network_fail_emergency_reset_callback(void)
 {
     _process_poweroff(1, "network_fail_reset");
 }
+
+void gps_ant_ok_callback(void)
+{
+
+}
+
+void gps_ant_nok_callback(void)
+{}
